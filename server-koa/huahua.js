36e1f26d323bb1b3fb3ea180961c68c8ca8751f5
savoygu <savoygu@163.com>
2017-12-29 19:09:01 +0800

    更新：增加 ip 获取

--- server-koa/huahua.js
+++ server-koa/huahua.js
@@ -1,145 +1,225 @@
-var _ = require('underscore')
-var $scope = {}
-{
+var projectList = [
+  {
+    'roleId': 'a6f9624b031e4affb92fbb12f5d9547b',
+    'agencyId': '312312312-321378129312-124512412231',
+    'agencyName': '中欧国际城',
+    'agencyType': '100000002',
+    'parentId': '3231d193-1fd8-e411-80c5-005056a61361',
+    'parentName': '金茂青岛',
+    'level': '4',
+    'parentLevel': '3'
+  },
+  {
+    'roleId': '54ea900d77ea4e6da1f27036f39a9852',
+    'agencyId': '3231d193-1fd8-e411-80c5-005056a61361',
+    'agencyName': '金茂青岛',
+    'agencyType': '100000003',
+    'parentId': '1e6395a7-376a-e411-80c1-005056a63151',
+    'parentName': '金茂北京',
+    'level': '3',
+    'parentLevel': '2'
+  },
+  {
+    'roleId': '12c6ddb8fd4d492baf8ff3bae92aa4f1',
+    'agencyId': '7d3e1bed-1fd8-e411-80c5-005056a61361',
+    'agencyName': '青岛金茂湾',
+    'agencyType': '100000002',
+    'parentId': '3231d193-1fd8-e411-80c5-005056a61361',
+    'parentName': '金茂青岛',
+    'level': '4',
+    'parentLevel': '3'
+  },
+  {
+    'roleId': '5dc98d242723435ebe4d9eab606b30c5',
+    'agencyId': 'aae7b20d-3a6a-e411-80c1-005056a63151',
+    'agencyName': '广渠金茂府',
+    'agencyType': '100000002',
+    'parentId': '1e6395a7-376a-e411-80c1-005056a63151',
+    'parentName': '金茂北京',
+    'level': '4',
+    'parentLevel': '2'
+  },
+  {
+    'roleId': 'a6f9624b031e4affb92fbb12f5d9547b',
+    'agencyId': 'e66e9021-43bd-e711-80fd-005056ac019c',
+    'agencyName': '金茂逸家园',
+    'agencyType': '100000002',
+    'parentId': '1e6395a7-376a-e411-80c1-005056a63151',
+    'parentName': '金茂北京',
+    'level': '4',
+    'parentLevel': '2'
+  }
+]
 
-}
-$scope.data = {
-  'roleList': [
-    {
-      'roleLevel': 1,
-      'roelName': '集团HSE',
-      'roelId': 111
-    },
-    {
-      'roleLevel': 2,
-      'roelName': '区域A',
-      'roelId': 222
-    },
-    {
-      'roleLevel': 3,
-      'roelName': '总包01',
-      'roelId': 333
-    }
-  ],
-  'fProjectList': [
-    {
-      'projectLevel': 3,
-      'currentId': 334,
-      'currentName': '项目A',
-      'parentId': '002',
-      'parentName': '区域A',
-      'roelId': '222',
-      'roelName': '区域A'
-    },
-    {
-      'projectLevel': 3,
-      'currentId': 335,
-      'currentName': '项目B',
-      'parentId': '002',
-      'parentName': '区域A',
-      'roelId': '222',
-      'roelName': '区域A'
-    },
-    {
-      'projectLevel': 3,
-      'currentId': '336',
-      'currentName': '项目B',
-      'parentId': '001',
-      'parentName': '区域B',
-      'roelId': '333',
-      'roelName': '总包01'
-    },
-    {
-      'projectLevel': 2,
-      'currentId': '336',
-      'currentName': '区域C',
-      'parentId': '011',
-      'parentName': '集团',
-      'roelId': '111',
-      'roelName': '集团HSE'
-    },
-    {
-      'projectLevel': 2,
-      'currentId': '001',
-      'currentName': '区域B',
-      'parentId': '011',
-      'parentName': '集团',
-      'roelId': '111',
-      'roelName': '集团HSE'
-    },
-    {
-      'projectLevel': 2,
-      'currentId': '002',
-      'currentName': '区域A',
-      'parentId': '011',
-      'parentName': '集团',
-      'roelId': '111',
-      'roelName': '集团HSE'
-    }
-  ],
-  QXList: [
-    {
-      'authFunctionId': 'change',
-      'roelId': 222
-    },
-    {
-      'authFunctionId': 'create',
-      'roelId': 111
-    },
-    {
-      'authFunctionId': 'reform',
-      'roelId': 333
-    }
-  ]
-}
-// 项目筛选
-$scope.projectList = []
-$scope.areaList = []
-$scope.groupList = []
-if ($scope.data.fProjectList.length > 0) {
-  _.each($scope.data.fProjectList, function (item, index) {
-    if (item.projectLevel == 3) {
-      $scope.projectList.push(item)
-    } else if (item.projectLevel == 2) {
-      $scope.areaList.push(item)
-    } else {
-      $scope.groupList.push(item)
-    }
-  })
-}
-
-// 权限合并
-function getAuthRole (roleId) {
-  // var deferred = $q.defer();
-  $scope.isHaveAuthRole = []
-  _.each($scope.data.roleList, function (item2, index2) {
-    var even = _.find($scope.data.QXList, function (item) {
-      return (item.roelId == item2.roelId && item.authFunctionId == roleId)
-    })
-    if (even) {
-      $scope.isHaveAuthRole.push(even)
-    }
-  })
-}
-getAuthRole('create')
-console.log($scope.isHaveAuthRole)
-
-// 查看某个项目底下的详情页
-function getTheRole (projectId) {
-  $scope.allRoles = []
-  var evens = _.filter($scope.data.fProjectList, function (item) {
-    return item.currentId == projectId
-  })
-  if (evens.length > 0) {
-    _.each(evens, function (item) {
-      var even = _.find($scope.data.QXList, function (item2) {
-        return (item.roelId == item2.roelId)
-      })
-      if (even) {
-        $scope.allRoles.push(even)
-      }
+var hash = {}
+var dataLevel2 = projectList.reduce(function (project, next) {
+  if (!hash[next.parentId] && next.parentLevel === '2') {
+    hash[next.parentId] = true
+    project.push({
+      agencyId: next.parentId,
+      agencyName: next.parentName,
+      level: next.parentLevel
     })
   }
+  return project
+}, [])
+
+var getTree = arr => {
+  const map = arr.reduce((acc, val) => {
+    acc[val.agencyId] = val
+    return acc
+  }, {})
+
+  const tree = []
+  arr.forEach(region => {
+    if (region.parentId) {
+      const parent = map[region.parentId]
+      if (!parent.children) {
+        parent.children = [region]
+      } else {
+        parent.children.push(region)
+      }
+    } else {
+      tree.push(region)
+    }
+  })
+
+  return { map, tree }
 }
-getTheRole('336')
-console.log($scope.allRoles)
+
+console.log(getTree(projectList.concat(dataLevel2)).tree)
+
+// [{
+//   agencyId: '',
+//   agencyName: '',
+//   level: 2,
+//   data: [
+//     {
+//       agencyId: '',
+//       agencyName: '',
+//       level: 3,
+//       data: [
+//         {
+//           agencyId: '',
+//           agencyName: '',
+//           level: 4
+//         }
+//       ]
+//     }
+//   ]
+// }]
+
+// 三层
+
+/* var dataLevel2 = []
+var dataLevel3 = []
+
+for (var i = 0; i < projectList.length; i++) {
+  var project = projectList[i]
+  var hasLevel2Repeat = dataLevel2.some(item => item.agencyId === project.parentId)
+  var hasLevel3Repeat = dataLevel3.some(item => item.agencyId === project.parentId)
+  var city = {
+    agencyId: project.parentId,
+    agencyName: project.parentName,
+    level: project.parentLevel,
+    data: []
+  }
+  if (!hasLevel2Repeat && project.parentLevel === '2') {
+    dataLevel2.push(city)
+  }
+  if (!hasLevel3Repeat && project.parentLevel === '3') {
+    dataLevel3.push(city)
+  }
+}
+
+function next (data, agencyId, level) {
+  for (var i = 0; i < projectList.length; i++) {
+    var project = projectList[i]
+    var item = {
+      agencyId: project.agencyId,
+      agencyName: project.agencyName,
+      agencyType: project.agencyType,
+      level: project.level
+    }
+    if (project.parentLevel === level && project.parentId === agencyId) { // 如果 agencyId = 2
+      data.push(item)
+    }
+  }
+}
+
+for (var j = 0; j < dataLevel2.length; j++) {
+  var itemLevel2 = dataLevel2[j]
+  next(itemLevel2.data, itemLevel2.agencyId, itemLevel2.level)
+}
+
+for (var k = 0; k < dataLevel3.length; k++) {
+  var itemLevel3 = dataLevel3[k]
+  next(itemLevel3.data, itemLevel3.agencyId, itemLevel3.level)
+}
+
+for (var l = 0; l < dataLevel2.length; l++) {
+  var data = dataLevel2[l].data
+  for (var m = 0; m < data.length; m++) {
+    for (var n = 0; n < dataLevel3.length; n++) {
+      console.log(data[m].agencyId, dataLevel3[n].agencyId)
+      if (data[m].agencyId === dataLevel3[n].agencyId) {
+        dataLevel2[l].data[m].data = dataLevel3[n].data
+        break
+      }
+    }
+  }
+} */
+
+// 两层
+
+// var dataLevel2 = []
+
+// for (var i = 0; i < projectList.length; i++) {
+//   var project = projectList[i]
+//   var hasLevel2Repeat = dataLevel2.some(item => item.agencyId === project.parentId)
+//   var city = {
+//     agencyId: project.parentId,
+//     agencyName: project.parentName,
+//     level: project.parentLevel,
+//     data: []
+//   }
+//   if (!hasLevel2Repeat && project.parentLevel === '2') {
+//     dataLevel2.push(city)
+//   }
+// }
+
+// console.log(dataLevel2)
+
+// function next (data, agencyId, level) {
+//   for (var i = 0; i < projectList.length; i++) {
+//     var project = projectList[i]
+//     var item = {
+//       agencyId: project.agencyId,
+//       agencyName: project.agencyName,
+//       agencyType: project.agencyType,
+//       level: project.level
+//     }
+//     if (project.parentLevel === level && project.parentId === agencyId) { // 如果 agencyId = 2
+//       data.push(item)
+//     }
+//   }
+// }
+
+// for (var j = 0; j < dataLevel2.length; j++) {
+//   var itemLevel2 = dataLevel2[j]
+//   next(itemLevel2.data, itemLevel2.agencyId, itemLevel2.level)
+// }
+
+// var hash = {}
+// dataLevel2 = projectList.reduce(function (project, next) {
+//   if (!hash[next.parentId] && next.parentLevel === '2') {
+//     hash[next.parentId] = true
+//     project.push({
+//       agencyId: next.parentId,
+//       agencyName: next.parentName,
+//       level: next.parentLevel,
+//       data: []
+//     })
+//   }
+//   return project
+// }, [])
--- server-koa/service/location.js
+++ server-koa/service/location.js
@@ -1,10 +1,28 @@
 const request = require('request')
+const superagent = require('superagent')
 const URL = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip='
 
-module.exports = function getIpInfo (ip) {
+const _location = module.exports = {}
+
+_location.getLocationInfo = function (ip) {
   return new Promise(function (resolve, reject) {
     request.get(URL + ip).then(function (response) {
       resolve(response.body)
     })
+  })
+}
+
+_location.getIp = function () {
+  return new Promise(function (resolve, reject) {
+    superagent
+      .get('http://ip.cn/')
+      .set('User-Agent', 'curl/7.31.1')
+      .end(function (err, res) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
   })
 }
