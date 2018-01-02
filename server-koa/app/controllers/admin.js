ba467394073b8785a37ae6a0a4253aad152d8299
savoygu <saifeizhoukou@163.com>
2018-01-03 00:31:06 +0800

    新增：更新留言

--- server-koa/app/controllers/admin.js
+++ server-koa/app/controllers/admin.js
@@ -1,27 +1,57 @@
-const message = require('../models/message')
+const _ = require('underscore')
+const Message = require('../models/message')
 const _service = require('../service')
 const { getMessages } = require('../service/message')
 
 module.exports = {
   getMessages: async (ctx) => {
-    let body = {code: '01', result: ''}
     try {
       const result = await getMessages(ctx)
-      body.result = result
-    } catch (e) {
-      body.code = '02'
-      body.result = e.message
-    } finally {
-      ctx.render('message/messages', {
+      ctx.render('message/list', {
         title: '留言列表',
-        data: body.result
+        data: result
       })
+    } catch (e) {
+      console.log('留言列表：', e)
     }
   },
 
   findMessage: async (ctx) => {},
 
-  destoryMessage: async (ctx) => {},
+  showMessage: async (ctx) => {
+    const id = ctx.params.id
+    try {
+      if (id) {
+        let message = await _service.findById(Message, id)
+        console.log(message)
+        ctx.render('message/update', {
+          title: '更新留言',
+          message
+        })
+      }
+    } catch (e) {
+      console.log('更新留言：', e)
+    }
+  },
 
-  replyMessage: async (ctx) => {}
+  updateMessage: async (ctx) => {
+    const id = ctx.request.body.message._id
+    const message = ctx.request.body.message
+    console.log(message)
+    let _message
+    try {
+      if (id) {
+        let oldMessage = await _service.findById(Message, id)
+        _message = _.extend(oldMessage, message)
+        const newMessage = await _message.save()
+        ctx.redirect('/admin/messages')
+      }
+    } catch (e) {
+      console.log('更新留言：', e)
+    }
+  },
+
+  replyMessage: async (ctx) => {},
+
+  destoryMessage: async (ctx) => {}
 }
--- server-koa/app/routes/admin.js
+++ server-koa/app/routes/admin.js
@@ -5,6 +5,8 @@ const adminController = require('../controllers/admin')
 function adminRouter (router) {
   router.get('/message', adminController.findMessage)
   router.get('/messages', adminController.getMessages)
+  router.get('/message/update/:id', adminController.showMessage)
+  router.post('/message/update', adminController.updateMessage)
   router.post('/message/reply', adminController.replyMessage)
   router.get('/message/withdraw', adminController.destoryMessage)
 }
