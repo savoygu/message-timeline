e8d9496d5701b453cb26d9066775bbc739935a37
savoygu <1029559886@qq.com>
2023-01-15 12:30:15 +0800

    重构：客户端部分移除 vue2 代码

--- client/vue2/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_URL: `"${process.env.VUE_APP_BASE_URL || 'http://localhost:3000/api/v1'}"`,
-  IMG_URL: `"${process.env.VUE_APP_IMG_URL || '//img.mt.gusaifei.com'}"`
-}
