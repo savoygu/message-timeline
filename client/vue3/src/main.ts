795257935ea34d02a83b60d33fefc427baa56697
savoygu <1029559886@qq.com>
2023-01-15 12:21:45 +0800

    重构：客户端部分只保留 vue2 代码

--- client/vue3/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/index.scss'
-
-const app = createApp(App)
-app.provide('imgURL', import.meta.env.VITE_APP_IMG_URL)
-app.provide('PAGE_SIZE', 32)
-app.mount('#app')
